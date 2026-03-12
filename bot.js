102f5149cafd32cb427346b0d69a4207d7aae481
marylonds217-ui <marylonds217@gmail.com>
2026-03-12 13:36:39 +0000

    Fix and Update

--- bot.js
+++ bot.js
@@ -1,3 +1,4 @@
+const { Client, GatewayIntentBits, EmbedBuilder, ChannelType, PermissionsBitField, SlashCommandBuilder, REST, Routes, ActionRowBuilder, ButtonBuilder, ButtonStyle, StringSelectMenuBuilder, ModalBuilder, TextInputBuilder, TextInputStyle } = require('discord.js');
 const { joinVoiceChannel, createAudioPlayer, createAudioResource, NoSubscriberBehavior, AudioPlayerStatus, entersState, VoiceConnectionStatus, StreamType } = require('@discordjs/voice');
 const fs = require('fs');
 const path = require('path');
